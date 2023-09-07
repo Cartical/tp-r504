7f9fc60091411b14490099a17a046d8d6e1d67bf
Calvin <calvin.cartier@univ-rouen.fr>
2023-09-07 09:56:52 +0200

    création fichier

---
+++ TP1/fonctions.py
@@ -0,0 +1,6 @@
+def puissance(a,b):
+	c=a**b
+	print("a carré b = "+ c)
+
+if not type(var) is int:
+	raise TypeError("Only integers are allowed")
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,10 @@
+import fonctions as f
+
 print("hello world")
+
+while True:
+	a=int(input("Saisir un nombre au clavier"))
+	b=a**2
+	print("le carré de"+ a + "est"+ b)
+	res = f.puissance(a,b)
+	print(res)
