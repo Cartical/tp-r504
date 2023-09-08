9a4a636a5c3b8d2980096641c5265556b2d3b357
Calvin <calvin.cartier@univ-rouen.fr>
2023-09-08 15:56:21 +0200

    badge

--- TP1/test.py
+++ TP1/test.py
@@ -7,7 +7,7 @@ def test_1():
 	assert f.puissance(2,2)==4 
 	assert f.puissance(-2,2)==4
 	assert f.puissance(2,0)==1 
-	assert f.puissance(-2,0)==-1, pass
+	assert f.puissance(-2,0)==-1
 	assert f.puissance(0,0)==1 
 	assert f.puissance(0,3)==0
 	assert f.puissance(0,-3)==0 
