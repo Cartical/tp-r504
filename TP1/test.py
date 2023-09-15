b7da7cbe61096177b424bc12c88688df2772cfe2
Calvin <calvin.cartier@univ-rouen.fr>
2023-09-15 09:08:44 +0200

    badge

--- TP1/test.py
+++ TP1/test.py
@@ -13,9 +13,9 @@ def test_1():
 		assert f.puissance(0,0)==1 
 		assert f.puissance(0,3)==0
 		assert f.puissance(0,-3)==0
-	except (AssertionError, ZeroDivisionError):
-		print("-2 puissance 0 = -1 et 0 puisssance 0 = 1")
-		pass
+	except (AssertionError, ZeroDivisionError) as msg:
+		print(msg, "-2 puissance 0 = -1 et 0 puisssance 0 = 1")
+
 	
 #def test_2():
 #	assert f.puissance(2,3)==-8
