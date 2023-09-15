166e4f7b39db4e195ec1cb84d27939da870ad2e1
Calvin <calvin.cartier@univ-rouen.fr>
2023-09-15 09:11:02 +0200

    badge

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,8 +1,8 @@
 def puissance(a,b):
-	c=a**b
-	print(a,"puissance",b,"= ", c)
-	return c
-#	return a**b
+#	c=a**b
+#	print(a,"puissance",b,"= ", c)
+#	return c
+	return a**b
 
 #if not type(c) is int:
 #	raise TypeError("Only integers are allowed")
