f6d72e24374ab4440389cf1189965776315f7216
Cartical <calvin.cartier@univ-rouen.fr>
2023-11-27 13:04:48 +0000

    TP9 UDP fonctionnel

--- TP9/ClientUDP.java
+++ TP9/ClientUDP.java
@@ -1,16 +1,28 @@
 import java.io.*;
 import java.net.*;
 
-public class ServeurUDP
+public class ClientUDP
 {
 	public static void main( String[] args )
 	{
-		InetAddress addr = InetAddress.getLocalHost();
-		System.out.println( "adresse=" +addr.getHostName() );
-		byte[] data = s.getBytes();
-		DatagramPacket packet = new DatagramPacket( data, data.lenght, addr, 1234 );
-		DatagramSocket sock = new DatagramSocket();
-		sock.send(packet);
-		sock.close();
+		try {		
+			InetAddress addr = InetAddress.getLocalHost();
+			System.out.println( "adresse=" +addr.getHostName() );
+			String s="Hello World";
+			byte[] data = s.getBytes();
+			DatagramPacket packet = new DatagramPacket( data, data.length, addr, 1234 );
+			DatagramSocket sock = new DatagramSocket();
+			sock.send(packet);
+			sock.receive(packet);
+			String str = new String(packet.getData() );
+			System.out.println( "reception de =" + str );
+
+
+			sock.close();
+		}
+		catch( Exception ex ) {
+			System.out.println( "erreur !" ) ;
+			ex.printStackTrace();
+		}
 	}
 }
--- TP9/ServeurUDP.java
+++ TP9/ServeurUDP.java
@@ -11,15 +11,10 @@ public class ServeurUDP
 			{
 				System.out.println( "-Waiting data" );
 				DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
-				try {
-					sock.receive(packet);
-				}
-				catch( Exception ex ) {
-					System.out.println( "erreur !" ) ;
-					ex.printStackTrace();
-				}
+				sock.receive(packet);
 				String str = new String(packet.getData() );
 				System.out.println( "str=" + str );
+				sock.send(packet);
 			}
 		}
 		catch( Exception ex ) {
