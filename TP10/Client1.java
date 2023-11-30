82bdd1718111289102fb855b24bb4af07526a7f1
Cartical <calvin.cartier@univ-rouen.fr>
2023-11-30 09:52:05 +0000

    TP10 client http java

---
+++ TP10/Client1.java
@@ -0,0 +1,36 @@
+// Client1.java 
+
+import java.io.*;
+import java.net.*;
+
+public class Client1
+{
+	public static void main( String[] args )
+	{
+		try {
+			Socket socket = new Socket( "localhost", 80 );
+			OutputStreamWriter osw = new OutputStreamWriter( socket.getOutputStream() );
+			InputStreamReader isw = new InputStreamReader( socket.getInputStream() );
+
+			BufferedWriter bufOut = new BufferedWriter( osw );
+			BufferedReader bufIn = new BufferedReader( isw );
+
+			String request = "GET / HTTP/1.0\r\n\r\n"; //requete HTTP
+			bufOut.write( request, 0, request.length() );
+			bufOut.flush();
+
+			String line = bufIn.readLine(); 	// lecture ligne par ligne 
+			while( line != null ) {			// tant qu'il y a des dpnnées reçus,
+				System.out.println( line );	// ... les afficher
+				line = bufIn.readLine();
+			}
+			bufIn.close();
+			bufOut.close();
+			socket.close();
+		}
+		catch( Exception ex ) {
+			System.out.println( "erreur !" ) ;
+			ex.printStackTrace();
+		}
+	}
+}
---
+++ TP10/compile.sh
@@ -0,0 +1,1 @@
+javac -classpath c:\temp\* *.java
