fbface3b4474ad7a83bae945b25933964172cdf9
Cartical <calvin.cartier@univ-rouen.fr>
2023-12-18 10:19:20 +0000

    exo 2 tp11

--- TP11/testchemin.sh
+++ TP11/testchemin.sh
@@ -7,7 +7,7 @@ echo "nb=${#tab[@]}"
 for (( i=1; i<${#tab[@]}; i++ ))
 do
 	chem=$chem"/"${tab[i]}
-	echo "i=$i chem=$chem"
+	#echo "i=$i chem=$chem"
 	if [ -e "$chem" ]; then
 		echo "Le chemin $chem existe"
 	else
